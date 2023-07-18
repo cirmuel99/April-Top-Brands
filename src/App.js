df31396c36bbfdc8519c8c0b0da286bec5301da2
Micheal Oki <mikebabs566@gmail.com>
2023-07-18 16:37:34 +0100

    'what we do ' component implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react'
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -11,21 +10,18 @@ import Team from './pages/Team'
 
 const App = () => {
   return (
-    <ChakraProvider>
-       <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Home />} />
-              <Route path='about' element={<About />} /> 
-              <Route path='services' element={<Services />} />
-              <Route path='contact' element={<Contact />} />
-              <Route path='team' element={<Team />} />
-              <Route path='*' element={<NoPage />} />
-            </Route>
-          </Routes>
-      </BrowserRouter>
-    </ChakraProvider>
-   
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='about' element={<About />} /> 
+          <Route path='services' element={<Services />} />
+          <Route path='contact' element={<Contact />} />
+          <Route path='team' element={<Team />} />
+          <Route path='*' element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
